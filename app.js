82643e10e2922fd2523180646e5fa95240a59ea7
Rafuca <rafa_tioo@hotmail.com>
2022-04-20 00:36:16 -0300

    Static back por ahora.

---
+++ app.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const path = require('path')
+
+const express = require('express');
+const res = require('express/lib/response');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false}));
+
+app.get('/',function (request, response){
+    response.render('index');
+});
+
+app.get('/restaurants',function (request, response){
+    response.render('restaurants');
+});
+
+app.get('/recommend',function (request, response){
+    response.render('recommend');
+});
+
+app.post('/recommend', function (request, response){
+    const restaurant = request.body;
+    const filePath = path.join(__dirname, 'data', 'restaurants.json')
+
+    const fileData = fs.readFileSync(filePath)
+    const storedRestaurants = JSON.parse(fileData)
+
+    storedRestaurants.push(restaurant);
+
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants))
+
+    response.redirect('/confirm');
+});
+
+app.get('/confirm',function (request, response){
+    response.render('confirm');
+});
+
+app.get('/about',function (request, response){
+    response.render('about');
+});
+
+app.listen(3000);
